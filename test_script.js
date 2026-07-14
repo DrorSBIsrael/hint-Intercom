430c0e4532a8dbdee9c55bcb1ff7c8e57b1db8bf
DrorSBIsrael <Dror@sbparking.co.il>
2026-07-14 12:16:35 +0300

    Delete test_script.js

--- test_script.js
+++
@@ -1,7 +0,0 @@
-
-        // Check permissions
-        const userRole = sessionStorage.getItem('intercom_user_role');
-        if (userRole !== 'admin') {
-            window.location.href = 'index.html';
-        }
-    
